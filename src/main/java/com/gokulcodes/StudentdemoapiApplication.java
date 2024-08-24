a1c96082d38b8585acd8944c3ca3af3776044d4e
gokulz <gokulakannan877@gmail.com>
2024-08-24 23:48:44 +0530

    first commit

---
+++ src/main/java/com/gokulcodes/StudentdemoapiApplication.java
@@ -0,0 +1,13 @@
+package com.gokulcodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdemoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentdemoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokulcodes/StudentdemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokulcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdemoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
